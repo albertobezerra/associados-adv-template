f9e70be54f6b9e59885b055174ccf26342fdbcf3
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2020-03-30 11:59:26 -0300

    tema

---
+++ footer.php
@@ -0,0 +1,33 @@
+<div class="footer">
+
+    <div class="container">
+      
+       <br><br><p class="esp">Venha conversar conosco!</p>
+       <h1 class="esp">Entre em contato</h1><br><br>
+        
+        
+        <div class="row">
+            <div class="col-sm-6 col-md-5 col-lg-6 footer1">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15792.995674474745!2d-35.987384432789!3d-8.278006270467612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a98bba4a5fecdb%3A0x36c5f5ad6210b74f!2sSeletto%20Eventos!5e0!3m2!1spt-BR!2sbr!4v1584630238108!5m2!1spt-BR!2sbr" width="100%" height="570" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+            <div class="col-sm-6 col-md-5 col-lg-6 footer2">
+              Formulario de email, aqui!
+            </div>
+        </div>
+            
+        
+    </div>
+
+</div>
+
+
+
+<?php wp_footer(); ?>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title><?php wp_title('-',true,'right'); bloginfo(); ?></title>
+
+	<link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/assets/images/logo1.png" type="image/x-icon">
+	<link href="https://fonts.googleapis.com/css?family=Oswald:700&display=swap" rel="stylesheet">
+
+	<?php wp_head(); ?>
+
+<body>
+
+
+	<div class="menu_principal">
+
+			<nav class="navbar navbar-expand-lg navbar-light bg-light bg-light-custom fixed-top">
+
+				<div class="container">
+
+					  <a class="navbar-brand" href=""><img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" /></a>
+
+					 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				    	<span class="navbar-toggler-icon"></span>
+				  	</button>
+
+					  <div class="collapse navbar-collapse navbar-collapse-custom" id="navbarSupportedContent">
+					    <ul class="navbar-nav ml-auto">
+
+	    	<?php require_once('assets/includes/class-wp-bootstrap-navwalker.php');  ?>
+					      
+			<?php
+			wp_nav_menu( array(
+				'menu'              => 'Menu',
+				'theme_location'    => 'menu-header',
+				'depth'             => 2,
+				'container'         => 'div',
+				'container_class'   => 'collapse navbar-collapse',
+				'container_id'      => 'navbarSupportedContent',
+				'menu_class'        => 'navbar-nav ml-auto',
+				'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+				'walker'            => new WP_Bootstrap_Navwalker(),
+			) );
+			?>
+
+					    </ul>
+					  </div>
+
+
+
+			  	</div>
+			</nav>
+
+	</div>
