332ec75881431674f32c6dc4ca09f56674d75d05
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2020-04-03 13:35:51 -0300

    Atualização
    Adição de imagem no footer e customização das cores finais

--- footer.php
+++ footer.php
@@ -1,21 +1,22 @@
 <div class="footer">
 
     <div class="container">
-      
-       <br><br><p class="esp">Venha conversar conosco!</p>
-       <h1 class="esp">Entre em contato</h1><br><br>
-        
-        
+
+        <br><br>
+        <p class="esp2">Venha conversar conosco!</p>
+        <h1 class="esp2">Entre em contato</h1><br><br>
+
+
         <div class="row">
             <div class="col-sm-6 col-md-5 col-lg-6 footer1">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15792.995674474745!2d-35.987384432789!3d-8.278006270467612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a98bba4a5fecdb%3A0x36c5f5ad6210b74f!2sSeletto%20Eventos!5e0!3m2!1spt-BR!2sbr!4v1584630238108!5m2!1spt-BR!2sbr" width="100%" height="570" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15792.995674474745!2d-35.987384432789!3d-8.278006270467612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a98bba4a5fecdb%3A0x36c5f5ad6210b74f!2sSeletto%20Eventos!5e0!3m2!1spt-BR!2sbr!4v1584630238108!5m2!1spt-BR!2sbr" width="100%" height="570" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
             <div class="col-sm-6 col-md-5 col-lg-6 footer2">
-              Formulario de email, aqui!
+                Formulario de email, aqui!
             </div>
         </div>
-            
-        
+
+
     </div>
 
 </div>
@@ -30,4 +31,5 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
