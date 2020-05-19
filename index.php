138b96babb5bfd5b1491689705af87cdccacbab6
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2020-05-19 09:53:32 -0300

    Atualização dos cards
    Animação dos cards

--- index.php
+++ index.php
@@ -50,34 +50,114 @@
 
         <div class="row">
 
-            <?php 
-            $args = array('post_type'=>'post', 'category_name'=>'atuacao', 'showposts'=>10 );
-            $my_posts = get_posts( $args );
-            if($my_posts) : foreach($my_posts as $post) : setup_postdata( $post );?>
-
             <div class="col-md-4">
-
-                <div class="card card-atuacao">
-
-                    <div class="card-body">
-                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao">
-                        <h5 class="card-title-atuacao"><?php the_title(); ?></h5>
-                        <p class="card-description-atuacao"><?php the_excerpt(); ?></p>
-                        <a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
-
+                <div class="card1 card-flip">
+                    <div class="card-front-familia">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito de Família</h5><br><br>                  
+                        </div>
                     </div>
 
-
+                    <div class="card-back-familia">
+                        <div class="card-body">
+                            <p class="card-text">É o ramo do Direito Civil que trata das relações familiares e das obrigações e direitos decorrentes dessas relações, tem como conteúdo os estudos do casamento, união estável, relações de parentesco, filiação, alimentos, bem de família, tutela, curatela e guarda.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
                 </div>
-
             </div>
 
-            <?php
-              endforeach;
-              endif;
-            ?>
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-tributario">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito Tributário</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-tributario">
+                        <div class="card-body">
+                            <p class="card-text">É o segmento do direito financeiro que define como serão cobrados dos cidadãos (contribuintes) os tributos e outras obrigações a ele relacionadas, para gerar receita para o Estado (fisco).</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-consumidor">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito do Consumidor</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-consumidor">
+                        <div class="card-body">
+                            <p class="card-text">Atuação preventiva, consultiva e litigiosa nas relações de consumo, o escritório proporciona assessoria completa aos seus clientes, desde a elaboração e a análise de contratos consumeristas, até a atuação em processos administrativos e judiciais.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-empresarial">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito Empresarial</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-empresarial">
+                        <div class="card-body">
+                            <p class="card-text">Ampla assessoria, estudo da realidade de cada empresa e vasta atuação na recuperação de crédito das empresas, por meio de ações de cobrança, de execução, monitórias e de indenização, primando por soluções extrajudiciais.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-previdenciario">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito Previdenciário</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-previdenciario">
+                        <div class="card-body">
+                            <p class="card-text">Benefícios de aposentadorias por invalidez, por idade, por tempo de contribuição, especial, pensão por morte, auxílio-doença, auxílio-acidente, salário-maternidade e benefícios assistenciais para pessoas portadoras de deficiência e idosos.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-trabalhista">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito Trabalhista</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-trabalhista">
+                        <div class="card-body">
+                            <p class="card-text">Orientação, prevenção e gerenciamento das questões trabalhistas, de modo a preservar e/ou minimizar os riscos para seus clientes. No caso de ajuizamentos, prima sempre pela celeridade na solução do litígio.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+
 
         </div>
+
+        <br>
 
         
 
@@ -92,7 +172,9 @@
 
         <br><br>
         <p class="esp">Últimas orientações</p>
-        <h1 class="esp">Orientações Jurídicas</h1><br><br><br>
+        <h1 class="esp">Orientações Jurídicas</h1>
+
+        <br><br><br>
 
         <div class="row">
 
@@ -130,7 +212,9 @@
               endif;
             ?>
 
-        </div><br>
+        </div>
+
+        <br>
 
 
         <a href="http://www.marcoslimaefilho.adv.br/orientacao"><button type="button" class="btn btn-primary btn-primary-custom">+ orientações</button></a>
--- page-nossaatuacao.php
+++ page-nossaatuacao.php
@@ -19,16 +19,101 @@
 
             <div class="col-md-4">
                 <div class="card1 card-flip">
-                    <div class="card-front">
+                    <div class="card-front-familia">
                         <div class="card-body"><br><br>
                         <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
                         <h5 class="card-title">Direito de Família</h5><br><br>                  
                         </div>
                     </div>
 
-                    <div class="card-back">
+                    <div class="card-back-familia">
                         <div class="card-body">
                             <p class="card-text">É o ramo do Direito Civil que trata das relações familiares e das obrigações e direitos decorrentes dessas relações, tem como conteúdo os estudos do casamento, união estável, relações de parentesco, filiação, alimentos, bem de família, tutela, curatela e guarda.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-tributario">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito Tributário</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-tributario">
+                        <div class="card-body">
+                            <p class="card-text">É o segmento do direito financeiro que define como serão cobrados dos cidadãos (contribuintes) os tributos e outras obrigações a ele relacionadas, para gerar receita para o Estado (fisco).</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-consumidor">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito do Consumidor</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-consumidor">
+                        <div class="card-body">
+                            <p class="card-text">Atuação preventiva, consultiva e litigiosa nas relações de consumo, o escritório proporciona assessoria completa aos seus clientes, desde a elaboração e a análise de contratos consumeristas, até a atuação em processos administrativos e judiciais.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-empresarial">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito Empresarial</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-empresarial">
+                        <div class="card-body">
+                            <p class="card-text">Ampla assessoria, estudo da realidade de cada empresa e vasta atuação na recuperação de crédito das empresas, por meio de ações de cobrança, de execução, monitórias e de indenização, primando por soluções extrajudiciais.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-previdenciario">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito Previdenciário</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-previdenciario">
+                        <div class="card-body">
+                            <p class="card-text">Benefícios de aposentadorias por invalidez, por idade, por tempo de contribuição, especial, pensão por morte, auxílio-doença, auxílio-acidente, salário-maternidade e benefícios assistenciais para pessoas portadoras de deficiência e idosos.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card1 card-flip">
+                    <div class="card-front-trabalhista">
+                        <div class="card-body"><br><br>
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/balancabranca.png" class="icone-atuacao"><br><br>
+                        <h5 class="card-title">Direito Trabalhista</h5><br><br>                  
+                        </div>
+                    </div>
+
+                    <div class="card-back-trabalhista">
+                        <div class="card-body">
+                            <p class="card-text">Orientação, prevenção e gerenciamento das questões trabalhistas, de modo a preservar e/ou minimizar os riscos para seus clientes. No caso de ajuizamentos, prima sempre pela celeridade na solução do litígio.</p><a href="https://api.whatsapp.com/send?phone=5581992030000&text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio!" class="card-link" target="_blank"><strong>Falar com o especialista!</strong></a>
                         </div>
                     </div>
                 </div>
@@ -41,13 +126,6 @@
 
     </div>
 </div>
-
-<!--Isso que devine o layout para cada págia-->
-<?php
-/*
-Template Name: Nossa atuação
-*/
-?>
 
 
 <?php get_footer(); ?>
